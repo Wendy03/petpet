85c9ede43f654e49300302e92ec60acb88cb3a2f
wendy03 <wendysu03@gmail.com>
2020-12-17 22:19:40 +0800

    新增: 預約Form modal JS

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -31,7 +31,45 @@ $(document).ready(function () {
 			$(".subscribeEmail").val('')
 		}
 	});
-	$('.datepicker').datepicker()
+	$(".carousel__inner a").click(function () {
+		$(".carousel__item--active img").attr("src", $(this).attr("href"));
+		return false;
+	});
+	$('#reservationModal').on('hidden.bs.modal', function () {
+		location.reload();
+	 })
+});
+
+$(function () {
+	$("#starDate").datepicker({
+		format: "yyyy-mm-dd",
+		autoclose: true,
+		startDate: "today",
+		todayHighlight: true
+	});
+	$("#endDate").datepicker({
+		format: "yyyy-mm-dd",
+		autoclose: true,
+		startDate: "today",
+		todayHighlight: true
+	});
 });
 
 
+let currentTab = 0;
+showTab(currentTab);
+function showTab(index) {
+	let tabItem = document.getElementsByClassName("tab");
+	tabItem[index].style.display = "block";
+}
+
+function nextPrev(index) {
+	let tabItem = document.getElementsByClassName("tab");
+	tabItem[currentTab].style.display = "none";
+	currentTab = currentTab + index;
+	showTab(currentTab);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	showTab(currentTab);
+}, false);
